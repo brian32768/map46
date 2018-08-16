a4f1d6af9fb26aae63275322f4ffb43f45800d9e
Brian Wilson <bwilson@co.clatsop.or.us>
2018-08-16 14:41:34 -0700

    initial

---
+++ xyz/README.md
@@ -0,0 +1,25 @@
+# map46 xyz
+
+# Install some packages
+
+npm install ol
+npm install --save-dev bootstrap
+npm install --save-dev less
+
+# This is so we can run gulp on the command line below.
+npm install --global gulp-cli
+npm install --save-dev gulp@^next
+
+# Fonts that the bootstrap book had me install
+npm install --save-dev glyphicons-halflings
+
+Bootstrap wants this but for some reason does not install it.
+npm install --save-dev popper.js@^1.14.4
+
+
+# test it (from a cmd not a bash)
+
+npm test
+
+http://localhost:1234/
+
---
+++ xyz/xyz.js
@@ -0,0 +1,28 @@
+﻿// xyz.js
+
+import 'ol/ol.css';
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import XYZ from "ol/source/XYZ";
+//import OSM from "ol/source/OSM";
+
+var esri = "https://services.arcgisonline.com/ArcGIS/rest/services/";
+var service = 'World_Street_Map';
+var map = new Map({
+    target: 'map',
+    layers: [
+        new TileLayer({
+            source: new XYZ({
+                attributions: 'Tiles © <a href="' + esri + service + '/MapServer">ArcGIS</a>',
+                url: esri + service + '/MapServer/tile/{z}/{y}/{x}'
+            })
+        })
+    ],
+    view: new View({
+        center: [-13775000, 5768000],
+        zoom: 11
+    })
+});
+
+console.log('xyz.js loaded');
+
