9994f2fab8d0fa5d6aeedf8c2393803d24272806
Brian Wilson <bwilson@co.clatsop.or.us>
2018-08-23 15:54:27 -0700

    added classes, ES6 style!

--- javascript_patterns/body.js
+++ javascript_patterns/body.js
@@ -1,24 +1,41 @@
 // body.js
 
 import {getDatestamp, datestamp} from './mock.js';
-
-var fill_our_container = function(s) {
-    document.getElementById('test').innerHTML = s;
-}
+import {Thing, TallThing} from './thing.js';
 
 function intervalHandler(evt) {
-    document.getElementById('header').innerHTML = getDatestamp();
+    document.getElementById('header').innerHTML = 'The turtle time is ' + getDatestamp();
 }
-
-// Here's an event handler that calls a function in mock.
-function clickHandler(evt) {
-    datestamp(fill_our_container);
-    console.log("click");
-};
 
 var btn = document.getElementById('ici');
 btn.onclick = clickHandler;
 
 var timed = setInterval(intervalHandler, 1000);
+
+// class tests
+// Get an instance of our object
+var obelisk = new Thing('Diviner');
+obelisk.length = 10;
+obelisk.width  = 10;
+obelisk.height = 100;
+
+// Show me its properties
+function clickHandler(evt) {
+    var content = '';
+    // enumerate the thing's properties.
+    for (var p in obelisk) {
+ 	content += p + ' = ' + obelisk[p] + '<br />';
+    }
+    document.getElementById('test').innerHTML = content;
+    console.log("click");
+}
+
+console.log('Object: ' + obelisk); // calls toString() method
+console.log('Object volume: ' + obelisk.volume());
+console.log('Thing foo: ' + Thing.foo() + ' bar: ' + Thing.bar);
+
+var t = new TallThing('Terrapene Mundi', 1000);
+console.log(t); // Dump the entire object, toString() not called.
+console.log(t.volume());
 
 console.log('body loaded');
