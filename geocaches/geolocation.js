e7051bb668a5af09ca7c435a9f718cd9fc6f4652
Brian Wilson <bwilson@co.clatsop.or.us>
2018-08-23 16:57:03 -0700

    fixed display of gps metadata

--- geocaches/geolocation.js
+++ geocaches/geolocation.js
@@ -12,6 +12,8 @@ String should go BLANK when track is turned off.
 
 */
 
+var tracking_toggled = false;
+
 function el(id) {
     return document.getElementById(id);
 }
@@ -26,6 +28,14 @@ export class Geolocator {
 	el('track').addEventListener('click', function() {
 	    el('track').classList.toggle("tracking_on");
 	    var tracking = el('track').classList.contains("tracking_on");
+	    if (tracking) {
+		// Center on map when we get the next position update
+		tracking_toggled = true;
+	    } else {
+		// Hide the position display
+		position_feature.setGeometry(null);
+		accuracy_feature.setGeometry(null);
+	    }
 	    geolocation.setTracking(tracking);
 	    console.log("Tracking " + tracking);
 	});
@@ -34,11 +44,39 @@ export class Geolocator {
 
 	// Update the position metadata display when the GPS data changes.
 	geolocation.on('change', function() {
-	    el('accuracy').innerText = geolocation.getAccuracy() + ' [m]';
-	    el('altitude').innerText = geolocation.getAltitude() + ' [m]';
-	    el('altitudeAccuracy').innerText = geolocation.getAltitudeAccuracy() + ' [m]';
-	    el('heading').innerText = geolocation.getHeading() + ' [rad]';
-	    el('speed').innerText = geolocation.getSpeed() + ' [m/s]';
+
+	    var posacc  = geolocation.getAccuracy();
+	    var alt     = geolocation.getAltitude();
+	    var altacc  = geolocation.getAltitudeAccuracy();
+	    var heading = geolocation.getHeading();
+	    var speed   = geolocation.getSpeed();
+
+	    /* Uncomment this to test with real numbers.
+	    posacc = 10;
+	    alt = 100;
+	    altacc = posacc * 2;
+	    heading = 3.14;
+	    speed = 6;
+	    */
+	    
+	    var msg = '';
+	    if (typeof posacc != 'undefined') {
+		msg += ' accuracy: ' + Math.round(posacc * 3.28084) + "'";
+	    }
+	    if (typeof alt != 'undefined') {
+		msg += ' elev: ' + Math.round(alt * 3.28084) + "'";
+	    }
+	    if (typeof altacc != 'undefined') {
+		msg += ' elev acc: ' + Math.round(altacc * 3.28084) + "'";
+	    }
+	    if (typeof heading != 'undefined') {
+		msg += ' head: ' + Math.round(heading * 57.29578) + '°';
+	    }
+	    if (typeof speed != 'undefined') {
+		msg += ' speed: ' + Math.round(speed * 2.2369362920544025) + ' mph';
+	    }
+	    
+	    el('gpsmeta').innerText = msg;
 	});
 
 	// Handle geolocation change of GPS error.
@@ -52,7 +90,13 @@ export class Geolocator {
 	geolocation.on('change:position', function() {
 	    var coordinates = geolocation.getPosition();
 	    position_feature.setGeometry(coordinates ? new Point(coordinates) : null);
-	    view.setCenter(coordinates);
+
+	    // If we just got position after tracking was activated, pan to show us at center of map.
+	    if (tracking_toggled) {
+		view.setCenter(coordinates);
+		tracking_toggled = false;
+		console.log("Panned to GPS");
+	    }
 	});
 
 	// Handle change of accuracy on map.
