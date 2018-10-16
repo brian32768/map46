1850bb02e82ebeb74881f7af2d094465ce2284b0
Brian Wilson <bwilson@co.clatsop.or.us>
2018-10-15 17:03:45 -0700

    sorta working partway

--- vectortiles/body.js
+++ vectortiles/body.js
@@ -1,39 +1,65 @@
-import Map from 'ol/Map.js';
-import View from 'ol/View.js';
+// body.js (vectortiles)
+//
+// For selection see http://openlayers.org/en/latest/examples/vector-tile-selection.html?q=vector+tile
+
+import {Map, View} from 'ol';
+import {transform as Transform} from 'ol/proj';
+import OSM from 'ol/source/OSM.js';
+import {Tile as TileLayer} from 'ol/layer';
+
 import MVT from 'ol/format/MVT.js';
-import VectorTileLayer from 'ol/layer/VectorTile.js';
+import VectorTile from 'ol/layer/VectorTile.js';
 import VectorTileSource from 'ol/source/VectorTile.js';
 import {Fill, Icon, Stroke, Style, Text} from 'ol/style.js';
 
-// Copied from ArcGIS.com
-const taxlots_url = "https://tiles.arcgis.com/tiles/l89P2qlKPxgrFDLw/arcgis/rest/services/Clatsop_DBO_taxlots_wm/VectorTileServer";
+import createMapboxStreetsStyle from './mapboxstyles.js';
 
-var selection = {};
+// Copied from ArcGIS.com
+const taxlotsUrl = "https://tiles.arcgis.com/tiles/l89P2qlKPxgrFDLw/arcgis/rest/services/Clatsop_DBO_taxlots_wm/VectorTileServer";
+
+var selection = {}; // list of selected features
+
+const startingLocation = {
+    center: Transform([-123.825, 46.181], 'EPSG:4326', 'EPSG:3857'), // astoria downtown
+    zoom: 13, minZoom: 10, maxZoom: 19
+}; 
+
+var osmStreetsLayer  = new TileLayer({
+    title: 'Streets',
+    type: 'base',
+    source: new OSM(),
+    crossOrigin: 'anonymous',
+    opacity: 1,
+    permalink: 'Streets',
+    visible: true,
+    zindex: 3
+});
 
 var id_property = '';
-var taxlots = taxlots_url + "/tile/{z}/{y}/{x}.pbf";
-var taxlot_layer = new VectorTileLayer({
+var taxlots = taxlotsUrl + "/tile/{z}/{y}/{x}.pbf";
+
+var taxlotsLayer = new VectorTile({
+    declutter: true,
     source: new VectorTileSource({
 	format: new MVT(),
 	url: taxlots
     }),
     style: function(feature) {
-	var selected = 1 ; // !!selection[feature.get(id_property)];
+	var selected = !!selection[feature.get(id_property)];
 	return new Style({
 	    stroke: new Stroke({
-		color: selected? 'rgba(200,20,20,0.8)' : 'gray',
-		width: selected? 2 : 1
+		color: selected? 'rgba(200,20,20, 0.8)' : 'rgba(20,20,20, 0.7)',
+		width: selected? 1 : 0.5
 	    }),
 	    fill: new Fill({
-		color: selected? 'rgba(200,20,20,0.1)' : 'rgba(20,20,20,0.9)'
+		color: selected? 'rgba(100,20,20, 0.5)' : 'rgba(20,20,20,0.1)'
 	    })
 	});
     }
 });
 
-/*
 import {mapbox_key} from "./keys.js";
-var mapbox_basemap = new VectorTileLayer({
+var mapboxBasemap = new VectorTile({
     declutter: true,
     source: new VectorTileSource({
 	attributions: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> ' +
@@ -42,21 +68,28 @@ var mapbox_basemap = new VectorTileLayer({
 	format: new MVT(),
 	url: 'https://{a-d}.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6/' + '{z}/{x}/{y}.vector.pbf?access_token=' + mapbox_key
     }),
-    style: createMapboxStreetsV6Style(Style, Fill, Stroke, Icon, Text)
+    style: createMapboxStreetsStyle(Style, Fill, Stroke, Icon, Text)
+});
+
+/*
+var esriBasemap = new VectorTile({
+    source: new VectorTileSource({
+	title: 'ESRI World Basemap',
+	type:  'base',
+	crossOrigin: 'anonymous',
+	format: new MVT(),
+	url: 'https://basemaps.arcgis.com/v1/arcgis/rest/services/World_Basemap/VectorTileServer/tile/{z}/{y}/{x}.pbf',
+    })
+//    style: createMapboxStreetsStyle(Style, Fill, Stroke, Icon, Text)
 });
 */
 
-// Does not work due to CORS erroer, need "Access-Control-Allow-Origin" added to header.
-var esri_basemap = new VectorTileLayer({
+/*
+var maptilerContourLayer = new VectorTile({
     source: new VectorTileSource({
-	format: new MVT(),
-	url: 'https://basemaps.arcgis.com/v1/arcgis/rest/services/World_Basemap/VectorTileServer/tile/{z}/{y}/{x}.pbf'
-    }),
-    style: createMapboxStreetsV6Style(Style, Fill, Stroke, Icon, Text)
-});
-
-var maptiler_contour_layer = new VectorTileLayer({
-    source: new VectorTileSource({
+	title: 'Contours',
+	type:  'base',
+	crossOrigin: 'anonymous',
 	format: new MVT(),
 	url: "https://maps.tilehosting.com/data/contours/{z}/{x}/{y}.pbf?key=oldTeLsOq24wfrAW6JQ5"
     }),
@@ -69,59 +102,65 @@ var maptiler_contour_layer = new VectorTileLayer({
 	});
     }
 });
+*/
 
-var maptiler_basemap = new VectorTileLayer({
+/*
+var maptilerBasemap = new VectorTile({
     source: new VectorTileSource({
+	title: 'Mapbox',
+	type:  'base',
+	crossOrigin: 'anonymous',
 	format: new MVT(),
 	url: "https://maps.tilehosting.com/data/v3/{z}/{x}/{y}.pbf?key=oldTeLsOq24wfrAW6JQ5"
     }),
-    style: createMapboxStreetsV6Style(Style, Fill, Stroke, Icon, Text)
+    style: createMapboxStreetsStyle(Style, Fill, Stroke, Icon, Text)
 });
+*/
+
+var layers = [
+    //osmStreetsLayer,
+    //maptilerBasemap,
+    //esriBasemap
+    mapboxBasemap,
+    //maptilerContourLayer,
+    taxlotsLayer
+];
 
 var map = new Map({
-    layers: [
-//	maptiler_basemap,
-	esri_basemap,
-//	mapbox_basemap,
-	maptiler_contour_layer,
-	taxlot_layer
-    ],
     target: 'map',
-    view: new View({
-	center: [-13784553, 5802546],
-	zoom: 11, minZoom: 10, maxZoom: 19
-    })
+    layers: layers,
+    view: new View(startingLocation)
 });
 
-//var selectElement = document.getElementById('type');
+//var selectElement = document.getElementById('type'); // singleselect | multiselect
+
 
 map.on('click', function(event) {
     var features = map.getFeaturesAtPixel(event.pixel);
     if (!features) {
         selection = {}; // clear selection
         // force redraw of layer style
-        taxlot_layer.setStyle(taxlot_layer.getStyle());
+        taxlotsLayer.setStyle(taxlotsLayer.getStyle());
+	console.log("Nothing here, feature(s) deselected!");
         return;
     }
     var i = 0;
     for (let f of features) {
-	console.log("feature " + i + ":" + f.getGeometry())
+	console.log("Feature " + i + ":" + f.getGeometry())
 	i++;
     }
     var feature = features[0];
-    var keys = Object.keys(feature);
-    console.log('keys = ' + keys); // show property names
+    var properties = feature.getProperties();
+    console.log('properties: ', JSON.stringify(properties,null,2));
+
     var fid = feature.get(id_property);
 
-    selection = {}; // clear any existing selection
-/*
-    if (selectElement.value === 'singleselect') {
+//    if (selectElement.value === 'singleselect') {
         selection = {};
-    }
-*/
-    // add selected feature to lookup
+//    }
+
     selection[fid] = feature;
 
     // force redraw of layer style
-    taxlot_layer.setStyle(taxlot_layer.getStyle());
+    taxlotsLayer.setStyle(taxlotsLayer.getStyle());
 });
---
+++ vectortiles/head.js
@@ -0,0 +1,13 @@
+// head.js (vectortiles)
+//
+import 'bootstrap/dist/css/bootstrap';
+
+// This does not work yet. Almost.
+// https://www.npmjs.com/package/glyphicons-halflings
+//import "glyphicons-halflings/css/glyphicons-halflings";
+
+import 'ol/ol.css';
+
+//import 'ol-ext/dist/ol-ext.css';
+//import 'ol-ext/control/Permalink.css';
+//import './vectortiles.css';
