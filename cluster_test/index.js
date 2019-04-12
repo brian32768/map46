9fe35b68ab28537cf903a94a66b8ba1f4ba0db2e
Brian H Wilson <bwilson@co.clatsop.or.us>
2019-04-11 17:04:45 -0700

    gpx drag and drop

--- cluster_test/index.js
+++ cluster_test/index.js
@@ -26,9 +26,7 @@ const clusterSource = new Cluster({
 });
 
 let styleCache = {};
-const clusterLayer = new VectorLayer({
-  source: clusterSource,
-  style: (feature) => {
+const clusterStyle = (feature) => {
     const size = feature.get('features').length;
     let style = styleCache[size];
     if (!style) {
@@ -38,110 +36,48 @@ const clusterLayer = new VectorLayer({
         }
         if (size <= 1) {
             style = new Style({
-                image: new CircleStyle({
-                  radius: 8,
-                  stroke: new Stroke({
-                    color: '#fff'
-                  }),
-                  fill: new Fill({
-                    color: '#2040AA'
-                  })
+              image: new CircleStyle({
+                radius: 8,
+                stroke: new Stroke({
+                  color: '#fff'
+                }),
+                fill: new Fill({
+                  color: '#2040AA'
                 })
+              })
             });
         } else {
             style = new Style({
-                image: new CircleStyle({
-                  radius: 10,
-                  stroke: new Stroke({
-                    color: '#fff'
-                  }),
-                  fill: new Fill({
-                    color: fcolor
-                  })
+              image: new CircleStyle({
+                radius: 10,
+                stroke: new Stroke({
+                  color: '#fff'
                 }),
-                text: new Text({
-                  text: size.toString(),
-                  fill: new Fill({
-                    color: '#fff'
-                  })
+                fill: new Fill({
+                  color: fcolor
                 })
-            });
-        }
+              }),
+              text: new Text({
+                text: size.toString(),
+                fill: new Fill({
+                  color: '#fff'
+                })
+              })
+          });
+      }
       styleCache[size] = style;
-    }
-    return style;
   }
+  return style;
+}
+
+const clusterLayer = new VectorLayer({
+  source: clusterSource,
+  style: clusterStyle
 });
 
 const osmLayer = new TileLayer({
     source: new OSM()
 });
-
-var defaultStyle = {
-    'Point': new Style({
-    	image: new CircleStyle({
-    	    fill: new Fill({
-    		color: 'rgba(0,128,128,0.5)'
-    	    }),
-    	    radius: 5,
-    	    stroke: new Stroke({
-    		color: '#ff0',
-    		width: 1
-    	    })
-    	})
-    }),
-    'LineString': new Style({
-    	stroke: new Stroke({
-    	    color: '#f00',
-    	    width: 3
-    	})
-    }),
-    'Polygon': new Style({
-    	fill: new Fill({
-    	    color: 'rgba(0,255,128,0.5)'
-    	}),
-    	stroke: new Stroke({
-    	    color: '#0ff',
-    	    width: 1
-       })
-    }),
-    'MultiPoint': new Style({
-    	image: new CircleStyle({
-    	    fill: new Fill({
-    		color: 'rgba(255,0,255,0.5)'
-    	    }),
-    	    radius: 5,
-    	    stroke: new Stroke({
-    		color: '#f0f',
-    		width: 1
-    	    })
-    	})
-    }),
-    'MultiLineString': new Style({
-    	stroke: new Stroke({
-    	    color: '#0f0',
-    	    width: 3
-    	})
-    }),
-    'MultiPolygon': new Style({
-    	fill: new Fill({
-    	    color: 'rgba(0,0,255,0.5)'
-    	}),
-    	stroke: new Stroke({
-    	    color: '#00f',
-    	    width: 1
-    	})
-    })
-};
-
-let styleFunction = (feature, resolution) => {
-    const featureStyleFunction = feature.getStyleFunction();
-    if (featureStyleFunction) {
-	    return featureStyleFunction.call(feature, resolution);
-    } else {
-	    return defaultStyle[feature.getGeometry().getType()];
-    }
-};
 
 dragAndDropInteraction.on('addfeatures', (event) => {
     pointSource.addFeatures(event.features)
@@ -150,14 +86,16 @@ dragAndDropInteraction.on('addfeatures', (event) => {
 
 const map = new Map({
     interactions: defaultInteractions().extend([dragAndDropInteraction]),
-    layers: [osmLayer, clusterLayer],
+    layers: [
+        osmLayer,
+        clusterLayer
+    ],
     target: 'map',
     view: new View({
         center: [0, 0],
         zoom: 2
     })
 });
-
 
 distance.addEventListener('input', function() {
   clusterSource.setDistance(parseInt(distance.value, 10));
