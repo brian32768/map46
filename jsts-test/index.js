81a5dafa91e04f8a33103345182b56b0dd35eef9
Brian H Wilson <bwilson@co.clatsop.or.us>
2019-04-18 12:28:54 -0700

    added jsts example

---
+++ jsts-test/index.js
@@ -0,0 +1,56 @@
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import GeoJSON from 'ol/format/GeoJSON.js';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
+import {fromLonLat} from 'ol/proj.js';
+import OSM from 'ol/source/OSM.js';
+import VectorSource from 'ol/source/Vector.js';
+import LinearRing from 'ol/geom/LinearRing.js';
+import {Point, LineString, Polygon, MultiPoint, MultiLineString, MultiPolygon} from 'ol/geom.js';
+
+const jsts = require('jsts');
+
+var source = new VectorSource();
+
+// This is GeoJSON
+// I had to rename it so that parcel would bundle it correctly
+import json from './assets/astoria.json'
+
+const astoria_ll = [-123.834,46.187];
+
+var format = new GeoJSON();
+var features = format.readFeatures(json, {featureProjection: 'EPSG:3857'});
+
+var parser = new jsts.io.OL3Parser();
+parser.inject(Point, LineString, LinearRing, Polygon, MultiPoint, MultiLineString, MultiPolygon);
+
+for (var i = 0; i < features.length; i++) {
+  var feature = features[i];
+  // convert the OpenLayers geometry to a JSTS geometry
+  var jstsGeom = parser.read(feature.getGeometry());
+
+  // create a buffer of 40 meters around each line
+  var buffered = jstsGeom.buffer(40);
+
+  // convert back from JSTS and replace the geometry on the feature
+  feature.setGeometry(parser.write(buffered));
+}
+
+source.addFeatures(features);
+
+var vectorLayer = new VectorLayer({
+    source: source
+});
+
+var rasterLayer = new TileLayer({
+    source: new OSM()
+});
+
+var map = new Map({
+    layers: [rasterLayer, vectorLayer],
+    target: document.getElementById('map'),
+    view: new View({
+      center: fromLonLat(astoria_ll),
+      zoom: 15
+    })
+});
